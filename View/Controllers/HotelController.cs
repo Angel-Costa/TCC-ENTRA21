1d91a39da53ed1cc2575a8e38e82d88211ca56db
NicollasLee <50496828+NicollasLee@users.noreply.github.com>
2019-08-27 16:43:13 -0300

    Alterado o LoginRepository e concluido o LoginController

--- View/Controllers/HotelController.cs
+++ View/Controllers/HotelController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Mvc;
-
 using Model;
 using Repository.Interfaces;
 using Repository.Repositories;
