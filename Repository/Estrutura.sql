c759e290b1e81214aeba7321a17a7f84b47284f3
LoeneSilva <49409909+LoeneSilva@users.noreply.github.com>
2019-08-29 15:52:58 -0300

    Estrutura modificada

--- Repository/Estrutura.sql
+++ Repository/Estrutura.sql
@@ -9,13 +9,20 @@ CREATE TABLE logins(
 CREATE TABLE clientes(
 	id INT PRIMARY KEY IDENTITY(1,1),
 
-	id_login INT
-	FOREIGN KEY(id_login) REFERENCES logins(id),
-
+	celular VARCHAR(100),
+	estado VARCHAR(100),
+	cidade VARCHAR(100),
+	cep VARCHAR(100),
+	bairro VARCHAR(100),
+	numero VARCHAR(100),
+	rua VARCHAR(100),
+	complemento VARCHAR(100),
 	nome VARCHAR(100),
 	cpf VARCHAR(100),
 	rg VARCHAR(100),
 	celular VARCHAR(100),
+	login VARCHAR(100),
+	senha VARCHAR(100),
 	registro_ativo BIT
 );
 
