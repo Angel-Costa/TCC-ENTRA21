244e7f6a0e668e2a3b7335a4915df8383485d193
LoeneSilva <49409909+LoeneSilva@users.noreply.github.com>
2019-09-11 16:15:23 -0300

    Update SugestaoController.cs

--- View/Controllers/SugestaoController.cs
+++ View/Controllers/SugestaoController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Model;
+using Repository.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +8,58 @@ using System.Web.Mvc;
 
 namespace View.Controllers
 {
+    [Route("sugestao")]
     public class SugestaoController : Controller
     {
-        // GET: Sugestao
+        private SugestaoRepository repository;
+
+        public SugestaoController()
+        {
+            repository = new SugestaoRepository();
+        }
+
         public ActionResult Index()
         {
+            List<Sugestao> sugestaos = repository.ObterTodos();
+            ViewBag.Sugestao = sugestaos;
             return View();
+        }
+
+        public ActionResult Cadastro()
+        {
+            SugestaoRepository sugestaoRepository = new SugestaoRepository();
+            List<Sugestao> sugestaos = repository.ObterTodos();
+            ViewBag.Sugestao = sugestaos;
+            return View();
+        }
+
+        [HttpPost,Route("cadastro")]
+        public ActionResult Cadastro(Sugestao sugestao)
+        {
+            var id = repository.Cadastro(sugestao);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet,Route("editar")]
+        public ActionResult Editar(int id)
+        {
+            var sugestao = repository.ObterPeloId(id);
+            ViewBag.Sugestao = sugestao;
+            return View();
+        }
+
+        [HttpPost,Route("editar")]
+        public ActionResult Editar(Sugestao sugestao)
+        {
+            var alterado = repository.Alterar(sugestao);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet,Route("apagar")]
+        public ActionResult Apagar(int id)
+        {
+            var apagou = repository.Apagar(id);
+            return RedirectToAction("Index");
         }
     }
 }
