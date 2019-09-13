d3636d60d3f4998ffc5c96f42bab79d0bca7502c
NicollasLee <50496828+NicollasLee@users.noreply.github.com>
2019-09-13 16:52:45 -0300

    Corrigido o Banco de dados e add as mascaras nos lugares nrecessarios

--- Repository/Estrutura.sql
+++ Repository/Estrutura.sql
@@ -1,12 +1,12 @@
-﻿	DROP TABLE IF EXISTS adminstradores, comodidades, hoteis , avaliacoes, clientes, sugestoes;
+﻿	DROP TABLE IF EXISTS administradores, comodidades, hoteis , avaliacoes, clientes, sugestoes;
 
 CREATE TABLE clientes(
 	id INT PRIMARY KEY IDENTITY(1,1),
 
-	celular VARCHAR(100),
+	celular VARCHAR(12),
 	estado VARCHAR(100),
 	cidade VARCHAR(100),
-	cep VARCHAR(9),
+	cep VARCHAR(10),
 	login VARCHAR(100),
 	bairro VARCHAR(100),
 	numero VARCHAR(20),
@@ -14,7 +14,7 @@ CREATE TABLE clientes(
 	complemento VARCHAR(100),
 	nome VARCHAR(100),
 	cpf VARCHAR(14),
-	rg VARCHAR(100),
+	rg VARCHAR(12),
 	senha VARCHAR(100),
 	registro_ativo BIT
 );
@@ -36,10 +36,10 @@ CREATE TABLE hoteis(
 	id INT PRIMARY KEY IDENTITY(1,1),
 	nome VARCHAR (50),
 	descricao VARCHAR (200),
-	valor_noite DECIMAL (9,2),
+	valor_noite DECIMAL (8,2),
 	estado VARCHAR (17),
 	cidade VARCHAR (50),
-	cep VARCHAR (9),
+	cep VARCHAR (10),
 	bairro VARCHAR (50),
 	numero VARCHAR (15),
 	rua VARCHAR (50),
@@ -58,16 +58,16 @@ CREATE TABLE comodidades(
 	registro_ativo BIT
 );
 
-CREATE TABLE adminstradores (
+CREATE TABLE administradores (
 	id INT PRIMARY KEY IDENTITY(1,1),
 
 	nome VARCHAR (50),
-	cpf VARCHAR (11),
+	cpf VARCHAR (14),
 	login VARCHAR (50),
 	senha VARCHAR(50),
 	
-	registro_ativo BIT,
-	privilegio VARCHAR
+	privilegio VARCHAR(20),
+	registro_ativo BIT
 );
 
 CREATE TABLE sugestoes(
@@ -84,15 +84,15 @@ CREATE TABLE sugestoes(
 );
 
 INSERT INTO hoteis (nome, descricao , valor_noite , estado, cidade, cep, bairro, numero ,rua, complemento, registro_ativo) VALUES
-('Dallas','profissional',12.0,'Santa Catarina', 'Blumenau',654541321,'Centro','654','por do sol','perto do shopping', 1 );
+('Dallas','profissional',12.0,'Santa Catarina', 'Blumenau','65.454-132','Centro','654','por do sol','perto do shopping', 1 );
 
-INSERT INTO comodidades (id_hotel,nome, registro_ativo) VALUES
+INSERT INTO comodidades (id_hotel, nome, registro_ativo) VALUES
 (1,'PLAYGROUND', 1),
 (1, 'refrigerador', 1);
 
 
-INSERT INTO adminstradores(nome, cpf, login, senha, privilegio) VALUES
-('Rafael', 12345678910, 'rafael@gmail.com', 'rafa123', 'cliente');
+INSERT INTO administradores(nome, cpf, login, senha, privilegio) VALUES
+('Rafael', '123.456.789-10', 'rafael@gmail.com', 'rafa123', 'cliente');
 
 INSERT INTO avaliacoes(nota, comentario, feedback) VALUES
 (10, 'Muito bom!', 'qualidade');
@@ -101,19 +101,11 @@ INSERT INTO sugestoes(nome, local, descricao, ponto_turistico, cidade, endereco)
 ('Morada do mar', 'em beira mar', 'piscina exclusiva para clientes, ideal para viajem em dupla', 'praia da lagoinha', 'bombinhas', 'Avenida Leopoldo Zarling, 1221, Bombinhas, CEP 88215-000, Brasil');
 
 INSERT INTO clientes(nome, celular, estado, cidade, cep, login, senha, bairro, numero, rua, complemento, cpf,rg) VALUES
-('Jose',997857239, 'SC', 'Blumenau',67130-170,'josefreitas@gmail.com', 'jose123', 'itoupava central', 201,'alex borchardt', 'proximo a weg', 12345678910, 12345678-9);
+('Jose','997857239', 'SC', 'Blumenau','67.130-170','josefreitas@gmail.com', 'jose123', 'itoupava central', '201','alex borchardt', 'proximo a weg', '123.456.789-10', '12.345.678-9');
 
 SELECT * FROM hoteis;
 SELECT * FROM comodidades ;	
-SELECT * FROM adminstradores ;
+SELECT * FROM administradores ;
 SELECT * FROM avaliacoes;
 SELECT * FROM sugestoes;
 SELECT * FROM clientes;
-
-
-TRUNCATE TABLE hoteis;
-TRUNCATE TABLE comodidades;
-TRUNCATE TABLE adminstradores;
-TRUNCATE TABLE avaliacoes;
-TRUNCATE TABLE sugestoes;
-TRUNCATE TABLE clientes;
